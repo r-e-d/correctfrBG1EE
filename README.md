ae7d42b96cc888f9e70a6dd4b600d6a35c6d31f9
Renaud Durlin <renaud.durlin@gmail.com>
2022-07-03 11:58:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Github Releases](https://img.shields.io/github/v/release/r-e-d/correctfrBG1EE)](https://github.com/r-e-d/correctfrBG1EE/releases/latest)
+
+<div align="center"><h1>Correction de la traduction de Baldur's Gate : Enhanced Edition</h1>
+
+<h3>Un mod développé sur <a href="https://www.baldursgateworld.fr">La Couronne de Cuivre</a> pour BG:EE (v2.6+) et SoD (v2.6+)<h3>
+
+</div><br>
+
+<div align="right"><a href="#top">Retour en haut de page</a></div>
---
+++ dist/index.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+
+class DialogFile {
+  constructor(filename) {
+    this.content = fs.readFileSync(filename, 'utf8');
+    this.index = 0;
+  }
+
+  getLine() {
+    if (this.index === this.content.length)
+      return undefined;
+
+    const begin = this.index++;
+    while (this.index < this.content.length) {
+      if (this.content[this.index] === '@' && this.content[this.index - 1] === '\n') {
+        break;
+      }
+      ++this.index;
+    }
+
+    const separator = this.content.indexOf('=', begin);
+    const nb = parseInt(this.content.substring(begin + 1, separator), 10);
+    const text = this.content.substring(separator + 1, this.index).trim();
+    return { nb, text };
+  }
+}
+
+function main() {
+  const ref = new DialogFile('tra/dialogref.tra');
+  const fix = new DialogFile('tra/dialog.tra');
+
+  const tph1FD = fs.openSync('correctfrbg1ee/lib/bg1ee.tph', 'w');
+  const tph2FD = fs.openSync('correctfrbg1ee/lib/sod.tph', 'w');
+  const traFD = fs.openSync('correctfrbg1ee/francais/correctfr.tra', 'w');
+
+  while (1) {
+    const refLine = ref.getLine();
+    const fixLine = fix.getLine();
+
+    if (refLine === undefined && fixLine === undefined)
+      break;
+
+    if (refLine === undefined)
+      throw { fixLine };
+
+    if (fixLine === undefined)
+      throw { refLine };
+
+    if (refLine.nb !== fixLine.nb)
+      throw { refLine, fixLine };
+
+    if (refLine.text !== fixLine.text) {
+      const offset = 1000000;
+      const tphFD = fixLine.nb < 34000 ? tph1FD : tph2FD;
+      fs.appendFileSync(tphFD, '\tSTRING_SET ~' + fixLine.nb + '~ @' + (offset + fixLine.nb) + '\n', 'utf8');
+      fs.appendFileSync(traFD, '@' + (offset + fixLine.nb) + ' = ' + fixLine.text + '\n', 'utf8');
+    }
+  }
+}
+
+try {
+  main();
+}
+catch (e) {
+  console.error(e);
+}
